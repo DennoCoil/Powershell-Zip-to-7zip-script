5a73e57146be1c5a3eeae116e646073dbd5e38c6
DennoCoil <36ada95e@opayq.com>
2021-02-19 00:16:38 -0500

    Major rewrite to make it recursively convert zip\rar to 7zip files, but still fails for some reason.

--- Zip to 7z script.ps1
+++ Zip to 7z script.ps1
@@ -1,36 +1,61 @@
-#Use this only for cases of compressing 1 file per 1 archive
-
-$TempFolderPath = ".\TEMPORARYFOLDER\"
-
-$IncludeFiles = @("*.zip", "*.rar")
-
-$ExcludeFiles = @("*.txt", "*.bat", "*.exe", "*.xml", "*.pdf", "*.png", "*.7z", "*.mp4", "*.zip", "*.jpg", "*.wav", "*.wad", "*.csv")
-
-$FoundItem = Get-ChildItem * -Include $IncludeFiles
+#  I spent over 8 hours on this piece of shit.  It took a ZIP file, made a blank folder, and an empty 7z file with only the first couple letters until a space sppeared.
+#  If you can figure this out, be my guest.
 
 
-#EXTRACTION
+    $homeLocation = Get-Location
+    $IncludeFiles = @("*.zip", "*.rar")
+    $foundItem = Get-ChildItem .\ -recurse | where {$_.extension -in ".zip",".rar"}
+    $7zipLocation = 'C:\Program Files\7-Zip\7z.exe'
+    $7zipArgsCompress = "a -mmt32 -mx9 -sdel $("$fileDirectory\$baseFileName").7z $("$tempFolderPath\*")"
+    $7zipArgsUnzip = "e $fullFilePath -o$tempFolderPath"
+    $temporaryFolderArray = @()
+    $originalFilesArray = @()
 
-New-Item -Type Directory -Path $TempFolderPath
+<#Function DebugInfo {
+    Write-Host " Full archive name: $originalFullName`n",
+    "Name of Archive:  $baseFileName`n",
+    "Direcotry file is in:  $fileDirectory`n",
+    "Full Path for Original Archive:  $fullFilePath`n",
+    "Temporary Folder Name:  $tempFolderName`n"
+    "Temporary Folder Path:  $tempFolderPath`n"
 
-ForEach ($zippedfile In $FoundItem) {
+}#>
 
-    Expand-7Zip -ArchiveFileName "$($zippedfile.Name)" -TargetPath $TempFolderPath
-    Remove-Item $zippedfile
-}
+#DebugInfo
 
-#COMPRESSION
+ForEach ($foundArchive in $foundItem) {
 
-$RomFile = Get-ChildItem -Path $TempFolderPath -Exclude $ExcludeFiles
+    $originalFullName = $foundArchive.Name
+    $baseFileName = $foundArchive.BaseName
+    $fileDirectory = $foundArchive.Directory
+    $fullFilePath = "$fileDirectory\$originalFullName"
+    $tempFolderName = $(get-random -minimum 10000000 -maximum 99999999)
+    $tempFolderPath = "$fileDirectory\$tempFolderName"
 
-ForEach ($Rom In $RomFile) {
-
-    $RomGet = $($Rom.BaseName + '.7z')
+    $originalFilesArray += "$fullFilePath"
     
-    <# -Path is the INPUT and -Archive FileName is the OUTPUT#>
-    Compress-7Zip -Path $Rom -ArchiveFileName $RomGet -CompressionLevel Ultra -CompressionMethod LZMA2 -Format SevenZip -DisableRecursion
+    New-Item -Type Directory -Path $tempFolderPath;
+
+    $temporaryFolderArray += "$tempFolderPath"
+
+    Start-Process -FilePath $7zipLocation -ArgumentList $7zipArgsUnzip -Wait
+    
+        #DebugInfo
+
+    Start-Process -FilePath $7zipLocation -ArgumentList $7zipArgsCompress -Wait
+
+        Write-Host "INFO:  $originalFullName has been recompressed to 7Zip." -ForegroundColor green
+
+        #DebugInfo
+
 }
 
-Move-Item -Path $($TempFolderPath + "\*.7z") -Destination ".."
+Read-Host -Prompt "Press any key to continue..."
 
-Remove-Item -Path $TempFolderPath -Recurse
+# This part does not work in ISE, only PowerShell.  Remove-Item is a broken piece of shit.
+#Write-Host -ForegroundColor green -Object ('INFO:  Recompression Script Complete.`nPress any key to continue.' -f [System.Console]::ReadKey().Key.ToString());
+
+$folders = 'C:\temp','C:\users\joe\foo'
+Get-ChildItem $temporaryFolderArray -r | Where {!$_.PSIsContainer} | Remove-Item -WhatIf
+
+Write-Host "Finally Done" -ForegroundColor DarkGreen
